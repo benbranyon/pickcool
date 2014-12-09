b9fa0c70f5476788f97506bc8ead26cf8cb9978a
Ben Allfree <ben@benallfree.com>
2014-12-09 02:39:26 -0800

    Refactor

--- html/js/controllers/MainCtrl.js
+++ html/js/controllers/MainCtrl.js
@@ -3,36 +3,6 @@ app.controller('MainCtrl', function ($state, $scope, $window, $location, api) {
   
   $scope.state = $state;
 
-  $scope.share = function () {
-    ezfb.ui(
-     {
-      method: 'feed',
-      name: 'angular-easyfb API demo',
-      picture: 'http://v2.7-beta.clipbucket.com/files/photos/2014/05/16/1400228168b6b742_l.jpg',
-      link: 'http://pick.cool/vote?/qclqht?p=preview',
-      description: 'Taylor Siwft is WAAAY better than Justin Beiber! Help me prove it by voting and sharing.'
-     },
-     function (res) {
-      console.log(res);
-      // res: FB.ui response
-     }
-    );
-  };
-  
-  $scope.vote = function(c) {
-    if($scope.current_user)
-    {
-      $('.candidate').removeClass('selected');
-      $('#c_'+c.id).addClass('selected');
-      $('#vote_step_1').modal();
-      $scope.current_selection = c;
-      c.vote_count++;
-      api.vote(c.id);
-    } else {
-     $('#login_dialog').modal();
-    }
-    console.log(c);
-  };
 
   $scope.$watch('current_user', function(newVal,oldVal,scope) {
     if($scope.current_user) {
