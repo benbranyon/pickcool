c962c41b10c87e344eea5f4c5177890cd17abe6f
Ben Allfree <ben@benallfree.com>
2014-12-11 04:53:24 -0800

    Revert "Removed main broadcast responder"
    This reverts commit d3ab1202bf41115b9acff1e457107a3e5e15c004.

--- html/js/controllers/MainCtrl.js
+++ html/js/controllers/MainCtrl.js
@@ -2,11 +2,17 @@ app.controller('MainCtrl', function ($state, $scope, $window, $location, api) {
   console.log('MainCtrl');
   $scope.state = $state;
   
-  $scope.$watch('current_user', function(newVal,oldVal,scope) {
-    if($scope.current_user) {
-      $('#login_dialog').modal('hide');
-    }
+  $scope.$on('go', function() {
+    console.log('go for MAIN');
+    $scope.$watch('current_user', function(newVal,oldVal,scope) {
+      if($scope.current_user) {
+        $('#login_dialog').modal('hide');
+      }
+    });
+
   });
+  
+
 
   $scope.candidates = [];
 });
