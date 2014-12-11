d3ab1202bf41115b9acff1e457107a3e5e15c004
Ben Allfree <ben@benallfree.com>
2014-12-11 04:52:21 -0800

    Removed main broadcast responder

--- html/js/controllers/MainCtrl.js
+++ html/js/controllers/MainCtrl.js
@@ -2,17 +2,11 @@ app.controller('MainCtrl', function ($state, $scope, $window, $location, api) {
   console.log('MainCtrl');
   $scope.state = $state;
   
-  $scope.$on('go', function() {
-    console.log('go for MAIN');
-    $scope.$watch('current_user', function(newVal,oldVal,scope) {
-      if($scope.current_user) {
-        $('#login_dialog').modal('hide');
-      }
-    });
-
+  $scope.$watch('current_user', function(newVal,oldVal,scope) {
+    if($scope.current_user) {
+      $('#login_dialog').modal('hide');
+    }
   });
-  
-
 
   $scope.candidates = [];
 });
