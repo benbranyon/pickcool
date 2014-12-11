d80bef37054db49a86067964328887dd1bcaa9a1
Ben Allfree <ben@benallfree.com>
2014-12-11 05:26:25 -0800

    Canonical URL bugfix

--- html/js/controllers/contests/ContestViewCtrl.js
+++ html/js/controllers/contests/ContestViewCtrl.js
@@ -4,6 +4,7 @@ app.controller('ContestViewCtrl', function($state, ezfb, $scope, $stateParams, a
   console.log($stateParams.contest_id);
   api.getContest($stateParams.contest_id, function(res) {
     $scope.contest = res.data;
+    $scope.contest.canonical_url = $location.protocol()+'://'+$location.host()+$state.href('contests-view', {'contest_id': $scope.contest.id, 'slug': $scope.contest.slug}),
     // Fix up contest data
     $scope.contest.highest_vote = 0;
     $scope.contest.total_votes = 0;
@@ -18,7 +19,7 @@ app.controller('ContestViewCtrl', function($state, ezfb, $scope, $stateParams, a
     ezfb.ui(
      {
       method: 'share',
-      href: $scope.contest.canonical_url,
+      href: $location.protocol()+'://'+$location.host()+$state.href('contests-view-voted', {'contest_id': $scope.contest.id, 'slug': $scope.contest.slug, 'user_id': $scope.current_user.id, 'candidate_id': $scope.contest.current_user_candidate_id}),
      },
      function (res) {
       console.log(res);
@@ -43,7 +44,6 @@ app.controller('ContestViewCtrl', function($state, ezfb, $scope, $stateParams, a
       }
       c.vote_count++
       $scope.contest.current_user_candidate_id = c.id;
-      $scope.contest.canonical_url = $location.protocol()+'://'+$location.host()+$state.href('contests-view-voted', {'contest_id': $scope.contest.id, 'slug': $scope.contest.slug, 'user_id': $scope.current_user.id, 'candidate_id': c.id});
       $scope.updateVoteProgress($scope.contest, c);
       api.vote(c.id);
     } else {
