a46d263f415995737bd91c0e2b18b275716d2356
Ben Allfree <ben@benallfree.com>
2014-12-17 12:13:45 -0800

    console.log fix

--- html/js/env.js
+++ html/js/env.js
@@ -1,11 +1,30 @@
 var API_ENDPOINT="/api/v1";
 var DEBUG=true;
 
+
+(function() {
+    var method;
+    var noop = function () {};
+    var methods = [
+        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+        'timeStamp', 'trace', 'warn'
+    ];
+    var length = methods.length;
+    var console = (window.console = window.console || {});
+
+    while (length--) {
+        method = methods[length];
+
+        // Only stub undefined methods.
+        if (!console[method]) {
+            console[method] = noop;
+        }
+    }
+}());
+
 if(DEBUG)
 {
   console.log("***DEBUGGING MODE ENABLED***");
-} else {
-  var console = {
-    'log': function() {},
-  };
 }
