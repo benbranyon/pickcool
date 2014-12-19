d62279311adf1fb4e12ceb201780837bcdf534e7
Ben Allfree <ben@benallfree.com>
2014-12-18 19:08:51 -0800

    Refresh script update

--- refresh.sh
+++ refresh.sh
@@ -1,5 +1,5 @@
-mysqldump use db126393_coolfactor > db.sql
-mysql -D use db126393_coolbeta < db.sql
+mysqldump db126393_coolfactor > db.sql
+mysql -D db126393_coolbeta < db.sql
 ./artisan migrate
 cp ../pick.cool/app/storage/data ./app/storage -rf
 
