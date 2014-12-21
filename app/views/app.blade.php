384a04bf8de43715f3b50a495ec5f0ed40c6f9ad
Ben Branyon <ben_branyon@yahoo.com>
2014-12-20 23:14:25 -0800

    Added google tracking code

--- app/views/app.blade.php
+++ app/views/app.blade.php
@@ -64,7 +64,7 @@
           <div flash-messages></div>
           <div ui-view ng-if="session_started"></div>
 
-          <div class="footer">
+          <footer class="footer">
             <div class="row">
               <div class="col-sm-12">
                 <ul class="pull-left nav nav-pills footer-nav">
@@ -86,7 +86,19 @@
                 </ul>
               </div>
             </div>
-          </div>
+
+            <script>
+              (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+              (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+              m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+              })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+              ga('create', 'UA-57868973-1', 'auto');
+              ga('send', 'pageview');
+            </script>
+
+          </footer>
+
         </div>
       </div>
     </div> <!-- // Container -->
