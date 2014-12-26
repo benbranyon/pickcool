ae44d910d728a308c63427a0822c743d74bdc3e8
Ben Allfree <ben@benallfree.com>
2014-12-25 17:25:16 -0800

    Refresh update

--- refresh.sh
+++ refresh.sh
@@ -2,6 +2,12 @@
 source ~/.bash_profile
 mysqldump db126393_coolfactor > db.sql
 mysql -D db126393_coolbeta < db.sql
+mysql -D db126393_coolbeta -e "update users set email=concat('user',id,'@benallfree.com');"
+mysqldump db126393_coolbeta > db.sql
 ./artisan migrate
-cp ../pick.cool/app/storage/data ./app/storage -rf
+rsync -avv ../pick.cool/app/storage/data ./app/storage
+
+# locally:
+scp mediatemple:~/domains/next.pick.cool/db.sql .
+rsync -avv mediatemple:~/domains/next.pick.cool/app/storage/data app/storage
 
