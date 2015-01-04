d1a5de0562a7dc52cc76c3e0c03c863179799cca
Ben Allfree <ben@benallfree.com>
2015-01-03 18:12:18 -0800

    Route cleanup

--- app/routes.php
+++ app/routes.php
@@ -122,33 +122,7 @@ Route::get("/{etag}/assets/{type}/{name}", 'AssetController@get');
 Route::get("/images/{id}/{size}", ['as'=>'image.view', 'uses'=>'AssetController@image']);
 
 
-Route::group([
-  'prefix' => 'api/v1',
-  'before'=>'origin',
-], function() {
-  Route::any('/my/contests', 'Api\V1\Controller@my_contests');
-  Route::any('/contests/create', 'Api\V1\ContestEditController@create');
-  Route::any('/contests/save', 'Api\V1\ContestEditController@save');
-  Route::any('/contests/join', 'Api\V1\ContestViewController@join');
-  Route::any('/user', 'Api\V1\UserController@get');
-  Route::any('/vote', 'Api\V1\VoteController@vote');
-  Route::any('/unvote', 'Api\V1\VoteController@unvote');
-  Route::any('/contests/top', 'Api\V1\ContestController@top');
-  Route::any('/contests/hot', 'Api\V1\ContestController@hot');
-  Route::any('/contests/new', 'Api\V1\ContestController@recent');
-  Route::any('/contests/local', 'Api\V1\ContestController@local');
-  Route::any('/contests/{id}', 'Api\V1\ContestController@get');
 
-  // Wildcard if no matched route
-  Route::any( '/', function(  ){
-    return ApiSerializer::ok();
-  })->where('all', '.*');
-  Route::any( '{all}', function( $uri ){
-    return ApiSerializer::ok();
-  })->where('all', '.*');
-});
-
-Route::get('/est/{contest_id}/{slug}/{user_id?}/{candidate_id?}', ['as'=>'contest.view', 'uses'=>'ContestViewController@view_old']);
 
 Route::get('/unfollow/{contest_id}/{candidate_id}', ['as'=>'contest.candidate.unfollow', 'uses'=>function($contest_id, $candidate_id) {
   return "You are no longer following {$c->contest->name} and will not receive any further updates about it.";
