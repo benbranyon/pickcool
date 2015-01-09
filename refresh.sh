ecb08eb0917ff762d34d8ccc655df7e99c687c51
Ben Allfree <ben@benallfree.com>
2015-01-08 16:04:42 -0800

    refresh update

--- refresh.sh
+++ refresh.sh
@@ -4,12 +4,11 @@ mysqldump db126393_coolfactor > db.sql
 mysql -D db126393_coolbeta < db.sql
 mysql -D db126393_coolbeta -e "update users set email=concat('user',id,'@benallfree.com');"
 ./artisan migrate
-./artisan standings:generate
 mysqldump db126393_coolbeta > db.sql
-rsync -avv ../pick.cool/app/storage/data/system/BenAllfree/LaravelStaplerImages/Image/images/ html/i
+rsync -avv ../pick.cool/html/i/ html/i
 
 # locally:
 scp mediatemple:~/domains/next.pick.cool/db.sql .
-rsync -avv mediatemple:~/domains/next.pick.cool/app/storage/data/system/BenAllfree/LaravelStaplerImages/Image/images/ html/i
+rsync -avv mediatemple:~/domains/next.pick.cool/html/i/ html/i
 
 
