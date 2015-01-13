1f97958a48fa6738a5b99b61205c028d57ce2a42
Ben Allfree <ben@benallfree.com>
2015-01-13 03:07:26 +0000

    Refresh update

--- refresh.sh
+++ refresh.sh
@@ -1,11 +1,10 @@
 !#/bin/bash
-source ~/.bash_profile
-mysqldump db126393_coolfactor > db.sql
-mysql -D db126393_coolbeta < db.sql
-mysql -D db126393_coolbeta -e "update users set email=concat('user',id,'@benallfree.com');"
+mysqldump -udb126393 -pl9mrWMj9IM -hexternal-db.s126393.gridserver.com db126393_coolfactor > db.sql
+mysql -uroot -pr00t -D coolbeta < db.sql
+mysql -uroot -pr00t -D coolbeta -e "update users set email=concat('user',id,'@benallfree.com');"
 ./artisan migrate
 mysqldump db126393_coolbeta > db.sql
-rsync -avv ../pick.cool/html/i/ html/i
+rsync -avv benallfree.com@205.186.179.182:~/domains/pick.cool/html/i/ html/i
 
 # locally:
 scp mediatemple:~/domains/next.pick.cool/db.sql .
