decae4d462bc2f885ff3595b399ca66b5a88a414
Ben Branyon <ben_branyon@yahoo.com>
2015-02-05 12:29:00 -0800

    fix validation

--- app/routes.php
+++ app/routes.php
@@ -278,49 +278,49 @@ Route::any('/join/{id}', ['before'=>'auth', 'as'=>'contest.join', 'uses'=>functi
       if ($validation->fails())
       {
           // Validation has failed.
+          Session::put('danger', 'Please enter your name, image, and valid music url including the http://.');
           return Redirect::to(r('contest.join', [$contest->id, 's'=>'bands']))->withErrors($validation);
       }
-    }
-
-    $file = Input::file('image');
-    $name = Input::get('name');
-    $music_url = Input::get('music_url');
-    if(!is_a($file, 'Symfony\Component\HttpFoundation\File\UploadedFile'))
-    {
-      Session::put('danger', 'Please upload an image file before continuing.');
-    } else {
-      try
+      $file = Input::file('image');
+      $name = Input::get('name');
+      $music_url = Input::get('music_url');
+      if(!is_a($file, 'Symfony\Component\HttpFoundation\File\UploadedFile'))
       {
-        $image = new Image();
-        $image->image = $file;
-        $image->save();
+        Session::put('danger', 'Please upload an image file before continuing.');
+      } else {
+        try
+        {
+          $image = new Image();
+          $image->image = $file;
+          $image->save();
 
-      } catch (Exception $e)
-      {
-        Session::put('danger', 'We were unable to recognize that image file format. Please try uploading a different file.');
+        } catch (Exception $e)
+        {
+          Session::put('danger', 'We were unable to recognize that image file format. Please try uploading a different file.');
+        }
+          $user = Auth::user();
+          if(!$user) return null;
+          $can = new Candidate();
+          $can->contest_id = $contest->id;
+          $can->user_id = $user->id;
+          $can->name = $name;
+          $can->music_url = $music_url;
+          $can->save();
+          $can = Candidate::find($can->id);
+          Message::create([
+            'user_id'=>$user->id,
+            'subject'=>"Pick joined - pending verification",
+            'body'=>View::make('messages.join_verify')->with([
+              'candidate'=>$can,
+              'contest'=>$can->contest,
+            ]),
+          ]);
+          $user->vote_for($can);
+
+          $image->candidate_id = $can->id;
+          $image->save();
+          return Redirect::to($can->canonical_url);
       }
-        $user = Auth::user();
-        if(!$user) return null;
-        $can = new Candidate();
-        $can->contest_id = $contest->id;
-        $can->user_id = $user->id;
-        $can->name = $name;
-        $can->music_url = $music_url;
-        $can->save();
-        $can = Candidate::find($can->id);
-        Message::create([
-          'user_id'=>$user->id,
-          'subject'=>"Pick joined - pending verification",
-          'body'=>View::make('messages.join_verify')->with([
-            'candidate'=>$can,
-            'contest'=>$can->contest,
-          ]),
-        ]);
-        $user->vote_for($can);
-
-        $image->candidate_id = $can->id;
-        $image->save();
-        return Redirect::to($can->canonical_url);
     }
   }
   if($state=='finalize')
