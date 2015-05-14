60616b2ff21caa013cc4157fdece3ae1c0382708
Ben Branyon <ben_branyon@yahoo.com>
2015-05-13 18:29:38 -0700

    fix landing page styling

--- app/views/landing.blade.php
+++ app/views/landing.blade.php
@@ -14,10 +14,11 @@
 
 @section('content')
 	<h1>Latest Pick Cool Events</h1>
+	<hr />
 	<h2>Musicians, Models, and Ink</h2>
 	<h3 style="margin-bottom:10px;">Feb 19th 2015</h3>
 	<a href="http://www.janugget.com/"><img style="max-width:150px;" alt="John Ascuaga's Nugget" class="img-responsive" src="/assets/img/nugget-color-logo.jpg" /></a>
-	<hr />
 	<div id="fb-root"></div><script>(function(d, s, id) {  var js, fjs = d.getElementsByTagName(s)[0];  if (d.getElementById(id)) return;  js = d.createElement(s); js.id = id;  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";  fjs.parentNode.insertBefore(js, fjs);}(document, 'script', 'facebook-jssdk'));</script><div class="fb-post" data-href="https://www.facebook.com/media/set/?set=a.339808079550788.1073741831.310629329135330&amp;type=1" data-width="320"><div class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/media/set/?set=a.339808079550788.1073741831.310629329135330&amp;type=1">Post</a> by <a href="https://www.facebook.com/the.pick.cool">Pick.Cool</a>.</div></div>
+	<hr />
 	<br />
 @stop
