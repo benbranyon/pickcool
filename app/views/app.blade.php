cefb65358bbc499902dda3f320fbe2728ef65d7d
Ben Allfree <ben@benallfree.com>
2015-06-30 16:28:32 -0700

    Console debug fix

--- app/views/app.blade.php
+++ app/views/app.blade.php
@@ -17,7 +17,7 @@
           while (length--) {
             method = methods[length];
             // Only stub undefined methods.
-            if (console[method]) {
+            if (!console[method]) {
                 console[method] = noop;
             }
           }
