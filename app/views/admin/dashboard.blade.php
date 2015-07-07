f33cc56d961273a32e787cf039fcd4ea8062c107
Ben Branyon <ben_branyon@yahoo.com>
2015-07-07 09:41:34 -0700

    remove queries from admin dashboard

--- app/views/admin/dashboard.blade.php
+++ app/views/admin/dashboard.blade.php
@@ -7,11 +7,5 @@
 @section('content')
 <h4>Marn'in</h4>
 
-<h5>App Stats</h5>
-<ul class="list-group">
-	<li class="list-group-item">Total Users: {{{count($data['users'])}}}</li>
-	<li class="list-group-item">New Users: {{{count($data['new_users'])}}}</li>
-	<li class="list-group-item">Total Votes: {{{count($data['votes'])}}}</li>
-	<li class="list-group-item">New Votes: {{{count($data['new_votes'])}}}</li>
-</ul>
+
 @stop
