9806237ded0ae7520186f3b0347d47be1342e8e1
Ben Branyon <ben_branyon@yahoo.com>
2015-07-07 09:49:04 -0700

    add users back

--- app/views/admin/dashboard.blade.php
+++ app/views/admin/dashboard.blade.php
@@ -8,5 +8,8 @@
 <h4>Marn'in</h4>
 
 <h5>App Stats</h5>
-
+<ul class="list-group">
+	<li class="list-group-item">Total Users: {{{count($data['users'])}}}</li>
+	<li class="list-group-item">New Users: {{{count($data['new_users'])}}}</li>
+</ul>
 @stop
