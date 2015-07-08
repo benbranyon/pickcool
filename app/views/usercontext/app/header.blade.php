e73e6f9bcfc14e667adaaf7475d4b672e9c5fcdc
Ben Allfree <ben@benallfree.com>
2015-07-08 22:43:31 +0000

    Redo header

--- app/views/usercontext/app/header.blade.php
+++ app/views/usercontext/app/header.blade.php
@@ -1,6 +1,13 @@
 <?php
 $u = Auth::user();
 $the_url = route(Input::get('r'), Input::get('p'));
+$login_url =         r(
+          'login', 
+          Input::get('r')==route('login') ? [] : [
+            'success'=>$the_url,
+            'cancel'=>$the_url
+          ]
+        );
 ?>
 <div class="clearfix">
   <ul class="subnav list-inline pull-right">
@@ -11,22 +18,17 @@ $the_url = route(Input::get('r'), Input::get('p'));
     @if($u)
       <li><a href="{{{r('inbox')}}}" class="{{{$u->has_unread_messages ? 'unread' : ''}}}"><i class="fa fa-envelope"></i></a></li>
       <li><a href="{{{r('logout', ['success'=>r('home')])}}}"><span class="glyphicon glyphicon-log-out" aria-hidden="true"></span></a></li>
-    @else
-      <li><a href="{{{
-        r(
-          'login', 
-          Input::get('r')==route('login') ? [] : [
-            'success'=>$the_url,
-            'cancel'=>$the_url
-          ]
-        )
-      }}}"><span class="glyphicon glyphicon-log-in" aria-hidden="true"></span></a></li>
     @endif
   </ul>
   <div class="clearfix"></div>
   <ul class="subnav list-inline pull-right">
       @if($u && $u->is_visible)
-        <li><a href="{{{route('leaderboard')}}}">You are in <span class="badge">{{nth($u->rank)}}</span> place with <span class="text-success"><span class="glyphicon glyphicon-star" aria-hidden="true"></span>{{$u->earned_points}}</span> earned and <span class="text-muted"><span class="glyphicon glyphicon-plus" aria-hidden="true"></span>{{$u->pending_points}}</span> pending points. What's this?</a>
+        <li><a href="{{{route('leaderboard')}}}">You are in <span class="badge">{{nth($u->rank)}}</span> place with <span class="text-success"><span class="glyphicon glyphicon-star" aria-hidden="true"></span>{{number_format($u->earned_points)}}</span> earned and <span class="text-muted"><span class="glyphicon glyphicon-plus" aria-hidden="true"></span>{{number_format($u->pending_points)}}</span> pending points. What's this?</a>
+      @else
+        <div class="alert alert-success">
+          {{number_format(User::count())}} users are earning points on Pick.Cool by voting for their favorites. Check out the 
+          <a href="{{{route('leaderboard')}}}">leaderboard</a> and <a href="{{{$login_url}}}">log in</a> to play.
+        </div>
       @endif
   </ul>
 </div>
