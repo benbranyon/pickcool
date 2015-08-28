8e2ec3cf9eb77c18fb2e804a19a0d0779e4e0bcb
Ben Branyon <ben_branyon@yahoo.com>
2015-08-27 20:54:17 -0700

    fix advertise page

--- app/views/legal/advertise.blade.php
+++ app/views/legal/advertise.blade.php
@@ -1,13 +1,18 @@
 @extends('app')
 
 @section('head')
+	<title>Advertise | pick.cool</title>
 @stop
 
 @section('content')
-<h1>Advertise with Pick.Cool</h1>
-<p>Interested in getting your business seen by the most active social network users on the planet? Pick.Cool receives over 50,000 visitors and 200,000 unique page views per month. The desire to vote for and support friends and family in our picks keeps users sharing and coming back. Don't miss another day of exposure to this audience.
-<p>Text link ads display at the top of each pick and are sold on a CPM basis of $100 per 10,000 impressions.
-<p>Please contact us at info@pick.cool to make arrangements. Bulk discounts are available.
-
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+			<h1>Advertise with Pick.Cool</h1>
+			<p>Interested in getting your business seen by the most active social network users on the planet? Pick.Cool receives over 50,000 visitors and 200,000 unique page views per month. The desire to vote for and support friends and family in our picks keeps users sharing and coming back. Don't miss another day of exposure to this audience.</p>
+			<p>Text link ads display at the top of each pick and are sold on a CPM basis of $100 per 10,000 impressions.</p>
+			<p>Please contact us at steve@wp-results.com to make arrangements. Bulk discounts are available.</p>
+		</div>
+	</div>
 </div>
 @stop
