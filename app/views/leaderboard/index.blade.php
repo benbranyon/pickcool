71bde30c515eefc3c77eeb3ae5989e10f8f32dc8
Ben Branyon <ben_branyon@yahoo.com>
2015-08-27 23:39:53 -0700

    update leaderboard layout

--- app/views/leaderboard/index.blade.php
+++ app/views/leaderboard/index.blade.php
@@ -1,17 +1,9 @@
 @extends('app')
 
 @section('content')
-<div class="leaderboard">
   <h1>Pick.Cool Leaderboard</h1>
   <hr/>
-  <h2>How to Play</h2>
-  <p><b class="text-success">Earn points by picking winners early.</b></p>
-  <p>Prove you are the arbiter of cool by voting on Pick.Cool.
-  <p>You earn points by voting. When someone votes for your pick after you did, you get a point. Earn maximum points by picking winners early.
-  <p>The <b class="text-success">Earned</b> score is from closed picks. The <b class="text-muted">Pending</b> score is from open picks and may still change.
-  <p><strong class="text-danger">Changing your vote will reset your Pending score.</strong></p>
-  <hr/>
-
+<div class="leaderboard">
   <table>
     <thead>
       <tr>
@@ -46,6 +38,15 @@
       </tr>
     @endforeach
   </table>
+  <hr />
+
+  <h2>How to Play</h2>
+  <p><b class="text-success">Earn points by picking winners early.</b></p>
+  <p>Prove you are the arbiter of cool by voting on Pick.Cool.
+  <p>You earn points by voting. When someone votes for your pick after you did, you get a point. Earn maximum points by picking winners early.
+  <p>The <b class="text-success">Earned</b> score is from closed picks. The <b class="text-muted">Pending</b> score is from open picks and may still change.
+  <p><strong class="text-danger">Changing your vote will reset your Pending score.</strong></p>
+
   {{$users->links()}}
 </div>
 @stop
